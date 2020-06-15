e7a91b2a1380f50a53520b31cdc52db3ee75e23b
richard <richardmachado.93@gmail.com>
2020-06-15 19:10:08 -0300

    contact form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,56 +1,98 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Avatar from '../../assets/download.jpg'
 import './styles.css'
-import TextField from '@material-ui/core/TextField';
-import { withStyles,makeStyles } from '@material-ui/core/styles';
 
-const CssTextField = withStyles({
-    root: {
-      "& label.Mui-focused": {
-        color: "blue"
-      },
-      "& .MuiInput-underline:after": {
-        borderBottomColor: "green"
-      },
-      "& .MuiOutlinedInput-root": {
-        "& fieldset": {
-          borderColor: "red"
-        },
-        "&:hover fieldset": {
-          borderColor: "yellow"
-        },
-        "&.Mui-focused fieldset": {
-          borderColor: "brown"
-        }
-      }
-    }
-  })(TextField);
 
-  const style = {
+
+
+export default function Index({title,id}) {
+
+  function handleMessage (e){
+    e.preventDefault();
+    alert("Mensagem Enviada")
+
+    const data = { name,
+    email,
+    subject,
+    message};
+   
+    
 
   }
+  const [name,setName]= useState('')
+  const [email,setEmail]= useState('')
+  const [subject,setSubject]= useState('')
+  const [message,setMessage]= useState('')
+   
   
+  const subjects = [
+    {
+      value: '',
+      label: 'Escolha um Assunto',
+    },
+    {
+      value: 'orcamento',
+      label: 'Orçamento',
+    },
+    {
+      value: 'interview',
+      label: 'Entrevista',
+    },
+    {
+      value: 'project',
+      label: 'Projeto',
+    },
+    {
+      value: 'others',
+      label: 'Outros',
+    },
+  ];
 
-
-export default function index({title,id}) {
-
+  
     return (
     <div className="container-contact" id ={id}>
             <h1>Me mande uma mensagem.</h1>
         <div className = "form">
-            <form> 
+            <form onSubmit={handleMessage}> 
                 <div className="container-input">
-                    <div className = "container-input-group">
-                        <TextField  color ='#44475A'id="outlined-nome" label="Nome" variant="outlined" />        
-                        <TextField id="outlined-basic" label="E-mail" variant="outlined" />                              
-                        <select className="input" id="exampleFormControlSelect1">
-                            <option>Orçamento</option>
-                            <option>Recrutamento</option>
-                            <option>Outros</option>                                                       
-                        </select>  
+                    <div className = "container-input-group"> 
+                      <section >
+                        <span >Nome</span>
+                        <input  
+                          value={name}
+                          onChange={(e)=>{setName(e.target.value)}}
+                         aria-describedby="emailHelp" placeholder="Digite o nome" />
+                      </section>
 
-                    </div>                     
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="4"></textarea>                   
+                      <section >
+                        <span >E-mail</span>
+                        <input 
+                          value={email}
+                          onChange={(e)=>{setEmail(e.target.value)}}
+                          type="email"  id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Entre com o e-mail" />
+                      </section>
+
+                      <section >
+                        <span >Assunto</span>
+                        <select 
+                            value={subject}
+                            onChange={(e)=>{setSubject  (e.target.value)}}
+                         >
+                          {subjects.map( (i) => (
+                            <option value={i.value}>{i.label}</option>
+                          ))}
+                        </select>
+                      </section>
+
+                    </div>   
+
+                      <section className="section-textarea">
+                        <span >Mensagem</span>
+                        <textarea 
+                          value={message}
+                          onChange={(e)=>{setMessage(e.target.value)}}
+                          placeholder="Digite a mensagem"></textarea>                   
+                      </section>          
                 </div>                       
                 <button className ="button" type="submit">Enviar</button>
             </form>
