e6197cf2c47c7bc67c6d7e35eb52146d4444009d
Richard Machado <richardmachado.93@outlook.com>
2020-06-30 20:39:56 -0300

    adjusted form

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -20,9 +20,15 @@ export default function index({id}) {
                         <img className = "rounded" src = {Avatar} alt = "" height="200px"/>
                     </Col>
                 </Row>
-                <Row >
-                    <p className = "description">  
-                    Atualmente sou responsável pela manutenção e implementação de novas funções para um sistema ERP usando a Linguagem Clipper e também para alguns aplicativos com JavaScript e React Native. Concluí há um ano o curso de Sistemas de Informação e ainda busco na prática o meu desenvolvimento profissional na área de                    TI. Antes disso fui responsável por uma loja virtual porém o foco era na parte comercial mas ainda sim, com o conhecimento de tecnologia, consegui contribuir para a melhoria de alguns dos processos da loja, agregando algumas ferramentas a rotina.                    Fiz um curso Técnico em Informática na ETEC Heliópolis em 2013. Estou cursando inglês para aprimorar fala ,leitura e escrita mas consigo me desenvolver bem com o inglês técnico.
+                <Row className = "description">
+                    <p >  
+                        Atualmente sou responsável pela manutenção e implementação de novas funções para um sistema ERP usando a Linguagem Clipper e também para alguns aplicativos com JavaScript e React Native.
+                    </p> 
+                    <p>  
+                    Concluí há um ano o curso de Sistemas de Informação e ainda busco na prática o meu desenvolvimento profissional na área de                    TI. 
+                    </p> 
+                    <p>  
+                    Antes disso fui responsável por uma loja virtual porém o foco era na parte comercial mas ainda sim, com o conhecimento de tecnologia, consegui contribuir para a melhoria de alguns dos processos da loja, agregando algumas ferramentas a rotina.                    Fiz um curso Técnico em Informática na ETEC Heliópolis em 2013. Estou cursando inglês para aprimorar fala ,leitura e escrita mas consigo me desenvolver bem com o inglês técnico.
                     </p> 
                 </Row>
            </Col> 
